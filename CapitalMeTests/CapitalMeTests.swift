c4acce8e31ce0c7d9f91168399816d899cb54d11
Olarn U <olarn.u@gmail.com>
2016-05-28 12:43:39 +0700

    Add unit test for SwiftyJSON

--- CapitalMeTests/CapitalMeTests.swift
+++ CapitalMeTests/CapitalMeTests.swift
@@ -7,30 +7,25 @@
 //
 
 import XCTest
+import SwiftyJSON
 @testable import CapitalMe
 
 class CapitalMeTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testJSONToCapitalInfo() {
+        let bundle = NSBundle.mainBundle()
+        let url = bundle.URLForResource("bangkok", withExtension: "json")
+        let data = NSData(contentsOfURL: url!)
+        let json = JSON(data: data!)
+        
+        XCTAssertTrue(json[0]["name"] == "Thailand")
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
